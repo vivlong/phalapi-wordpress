23a9b5c3c30c7c0941125c70e447672a8ccc782d
JamesLiao <vivlonglz@gmail.com>
2020-12-09 23:47:47 +0800

    update

--- src/Lite.php
+++ src/Lite.php
@@ -2,6 +2,8 @@
 
 namespace PhalApi\Wordpress;
 
+use PhalApi\Wordpress\HttpClient\HttpClientException;
+
 /**
  * Wordpress操作类.
  */
@@ -18,14 +20,17 @@ class Lite
         if (null == $this->config) {
             $this->config = $di->config->get('app.Wordpress');
         }
-
-        $wordpress = new Client(
-            $this->config['url'],
-            $this->config['api_key'],
-            $this->config['api_secret'],
-            $this->config['options']
-        );
-        $this->instance = $wordpress;
+        try {
+            $wordpress = new Client(
+                $this->config['url'],
+                $this->config['api_key'],
+                $this->config['api_secret'],
+                $this->config['options']
+            );
+            $this->instance = $wordpress;
+        } catch (Exception $e) {
+            $di->logger->error('Wordpress', ['error' => $e->getMessage()]);
+        }
     }
 
     public function getConfig()
@@ -38,32 +43,70 @@ class Lite
         return $this->instance;
     }
 
+    private function request($method = 'get', $route = '/', $parameters = [])
+    {
+        $di = \PhalApi\DI();
+        $wp = $this->instance;
+        if (!empty($wp)) {
+            try {
+                $results = null;
+                switch ($method) {
+                    case 'post':
+                        $results = $wp->post($route, $parameters);
+                        break;
+                    case 'delete':
+                        $results = $wp->delete($route, $parameters);
+                        break;
+                    default:
+                        $results = $wp->get($route, $parameters);
+                }
+
+                return $results;
+            } catch (HttpClientException $e) {
+                $lastRequest = $wp->http->getRequest();
+                $lastResponse = $wp->http->getResponse();
+                $rs = json_decode($lastResponse->getBody());
+                if ($rs && 400 == $rs->data->status) {
+                    return $rs;
+                } else {
+                    $di->logger->error('Wordpress', $method.' # '.$route, ['request' => $lastRequest->getBody()]);
+                    $di->logger->error('Wordpress', $method.' # '.$route, ['response' => $lastResponse->getBody()]);
+                }
+                $di->logger->error('Wordpress', $method.' # '.$route, ['error' => $e->getMessage()]);
+
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
     /**
      * Posts.
      */
     public function listPosts($args = [])
     {
-        return $this->instance->get('posts', $args);
+        return $this->request('get', 'posts', $args);
     }
 
     public function retrievePost($id, $args = [])
     {
-        return $this->instance->get('posts/'.$id, $args);
+        return $this->request('get', 'posts/'.$id, $args);
     }
 
     public function createPost($args = [])
     {
-        return $this->instance->post('posts', $args);
+        return $this->request('post', 'posts', $args);
     }
 
     public function updatePost($id, $args = [])
     {
-        return $this->instance->post('posts/'.$id, $args);
+        return $this->request('post', 'posts/'.$id, $args);
     }
 
     public function deletePost($id, $args = [])
     {
-        return $this->instance->delete('posts/'.$id, $args);
+        return $this->request('delete', 'posts/'.$id, $args);
     }
 
     /**
@@ -71,12 +114,12 @@ class Lite
      */
     public function listPages($args = [])
     {
-        return $this->instance->get('pages', $args);
+        return $this->request('get', 'pages', $args);
     }
 
     public function retrievePage($id, $args = [])
     {
-        return $this->instance->get('pages/'.$id, $args);
+        return $this->request('get', 'pages/'.$id, $args);
     }
 
     /**
@@ -84,27 +127,27 @@ class Lite
      */
     public function listCategories($args = [])
     {
-        return $this->instance->get('categories', $args);
+        return $this->request('get', 'categories', $args);
     }
 
     public function retrieveCategory($id, $args = [])
     {
-        return $this->instance->get('categories/'.$id, $args);
+        return $this->request('get', 'categories/'.$id, $args);
     }
 
     public function createCategory($args = [])
     {
-        return $this->instance->post('categories', $args);
+        return $this->request('post', 'categories', $args);
     }
 
     public function updateCategory($id, $args = [])
     {
-        return $this->instance->post('categories/'.$id, $args);
+        return $this->request('post', 'categories/'.$id, $args);
     }
 
     public function deleteCategory($id, $args = [])
     {
-        return $this->instance->delete('categories/'.$id, $args);
+        return $this->request('delete', 'categories/'.$id, $args);
     }
 
     /**
@@ -112,12 +155,12 @@ class Lite
      */
     public function listTags($args = [])
     {
-        return $this->instance->get('tags', $args);
+        return $this->request('get', 'tags', $args);
     }
 
     public function retrieveTag($id, $args = [])
     {
-        return $this->instance->get('tags/'.$id, $args);
+        return $this->request('get', 'tags/'.$id, $args);
     }
 
     /**
@@ -125,12 +168,12 @@ class Lite
      */
     public function listTaxonomies($args = [])
     {
-        return $this->instance->get('taxonomies', $args);
+        return $this->request('get', 'taxonomies', $args);
     }
 
     public function retrieveTaxonomy($taxonomy, $args = [])
     {
-        return $this->instance->get('taxonomies/'.$taxonomy, $args);
+        return $this->request('get', 'taxonomies/'.$taxonomy, $args);
     }
 
     /**
@@ -138,12 +181,12 @@ class Lite
      */
     public function listComments($args = [])
     {
-        return $this->instance->get('comments', $args);
+        return $this->request('get', 'comments', $args);
     }
 
     public function retrieveComment($id, $args = [])
     {
-        return $this->instance->get('comments/'.$id, $args);
+        return $this->request('get', 'comments/'.$id, $args);
     }
 
     /**
@@ -151,27 +194,27 @@ class Lite
      */
     public function listMedia($args = [])
     {
-        return $this->instance->get('media', $args);
+        return $this->request('get', 'media', $args);
     }
 
     public function retrieveMedia($id, $args = [])
     {
-        return $this->instance->get('media/'.$id, $args);
+        return $this->request('get', 'media/'.$id, $args);
     }
 
     public function createMedia($args = [])
     {
-        return $this->instance->post('media', $args);
+        return $this->request('post', 'media', $args);
     }
 
     public function updateMedia($id, $args = [])
     {
-        return $this->instance->post('media/'.$id, $args);
+        return $this->request('post', 'media/'.$id, $args);
     }
 
     public function deleteMedia($id, $args = [])
     {
-        return $this->instance->delete('media/'.$id, $args);
+        return $this->request('delete', 'media/'.$id, $args);
     }
 
     /**
@@ -179,7 +222,7 @@ class Lite
      */
     public function listSearchResults($args = [])
     {
-        return $this->instance->get('search', $args);
+        return $this->request('get', 'search', $args);
     }
 
     /**
@@ -187,19 +230,24 @@ class Lite
      */
     public function listUsers($args = [])
     {
-        return $this->instance->get('users', $args);
+        return $this->request('get', 'users', $args);
     }
 
     public function retrieveUser($id, $args = [])
     {
-        return $this->instance->get('users/'.$id, $args);
+        return $this->request('get', 'users/'.$id, $args);
     }
 
     /**
      * Custom.
      */
-    public function getCustom($route, $args = [])
+    public function get($route, $args = [])
     {
-        return $this->instance->get($route, $args);
+        return $this->request('get', $route, $args);
+    }
+
+    public function post($route, $args = [])
+    {
+        return $this->request('post', $route, $args);
     }
 }
